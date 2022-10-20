9cd15f65b6152b875a5b2d4382daab909970c1e5
Lawrence Chan <lawrence.c@hotmail.co.uk>
2022-10-20 13:17:09 -0700

    implement categories page for admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  
   root to: 'products#index'
   
   resources :about, only: [:index]
@@ -17,6 +18,10 @@ Rails.application.routes.draw do
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
+  end
+
+  namespace :admin do
+    resources :categories
   end
 
   # The priority is based upon order of creation: first created -> highest priority.
