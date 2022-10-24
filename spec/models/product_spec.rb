3eea5ed0cb13f066e48d8084b0f1ab48e6d421a9
Lawrence Chan <lawrence.c@hotmail.co.uk>
2022-10-24 13:49:04 -0700

    add testing for Products model

---
+++ spec/models/product_spec.rb
@@ -0,0 +1,46 @@
+require 'rails_helper'
+
+RSpec.describe Product, type: :model do
+  describe 'Validations' do
+    it 'should have a name present' do
+      @category = Category.new({name: 'test category'})
+      @product = @category.products.new({
+        name: "test flower",
+        price: 1000,
+        quantity: 5
+        })
+      @product.save!
+      expect(@product.name).to be_present
+    end
+    it 'should have a price present' do
+      @category = Category.new({name: 'test category'})
+      @product = @category.products.new({
+        name: "test flower",
+        price: 1000,
+        quantity: 5
+        })
+      @product.save!
+      expect(@product.price).to be_present
+    end
+    it 'should have a quantity present' do
+      @category = Category.new({name: 'test category'})
+      @product = @category.products.new({
+        name: "test flower",
+        price: 1000,
+        quantity: 5
+        })
+      @product.save!
+      expect(@product.quantity).to be_present
+    end
+    it 'should have a category present' do
+      @category = Category.new({name: 'test category'})
+      @product = @category.products.new({
+        name: "test flower",
+        price: 1000,
+        quantity: 5
+        })
+      @product.save!
+      expect(@product.category).to be_present
+    end
+  end
+end
