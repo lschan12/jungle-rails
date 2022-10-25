9b915a2531f266173a8edd384c2d7c2afc6fff50
Lawrence Chan <lawrence.c@hotmail.co.uk>
2022-10-25 15:38:44 -0700

    implement testing for add to cart button

--- cypress/integration/add_to_cart.spec.js
+++ cypress/integration/add_to_cart.spec.js
@@ -4,14 +4,14 @@ describe('Jungle', () => {
     // so we must tell it to visit our website with the `cy.visit()` command.
     // Since we want to visit the same URL at the start of all our tests,
     // we include it in our beforeEach function so that it runs before each test
-    cy.visit('http://localhost:3000')
+    cy.visit('/')
   })
-  it("should go to product detail page by clicking on a product", () => {
+  it("should add product to cart when clicking add to cart button", () => {
     cy.get(".products article")
       .first()
-      .find('img') 
-      .click()
-    cy.contains("The Scented Blade is an extremely rare")
-      .should("exist")
+      .find('button') 
+      .click({ force: true})
+    cy.get("[href='/cart']")
+      .contains("(1)")
   });
 })
